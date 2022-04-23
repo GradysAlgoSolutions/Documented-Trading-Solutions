d245ff6cf892b4a88c0a05e94c14d41c07fd1ef9
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2022-04-23 13:47:59 +0000

    Introduce backtester integration tests

---
+++ tests/test_backtester.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+import pandas as pd
+
+
+class TestBacktester:
+    """Unit tests for backtester."""
+
+    def test_placeholder(self):
+        """Placeholder test — replace with real tests."""
+        # TODO: Implement tests for backtester
+        assert True
+
+    def test_init(self):
+        """Test object initialisation."""
+        # TODO: Test that class initialises without errors
+        pass
+
+    def test_fit(self):
+        """Test fit method."""
+        # TODO: Create sample data and verify fit() runs
+        pass
