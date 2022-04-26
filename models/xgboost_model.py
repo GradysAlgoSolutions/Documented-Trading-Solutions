8b2c2f84792f4bf1e0c96f3c0299496e33f09857
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2022-04-26 16:22:41 +0000

    Set up XGBoost trading model

---
+++ models/xgboost_model.py
@@ -0,0 +1,68 @@
+"""Xgboost Trading Model
+
+TODO: Replace stub with actual implementation.
+"""
+import numpy as np
+import pandas as pd
+from typing import Optional, Tuple, Dict, Any
+import xgboost as xgb
+from sklearn.metrics import accuracy_score
+
+
+class XGBoostModel:
+    """
+    Xgboost Trading Model.
+
+    Parameters
+    ----------
+    (see __init__ for parameters)
+    """
+
+    def __init__(self, learning_rate: float = 1e-3, epochs: int = 100):
+        """Initialise XGBoost trading model."""
+        # TODO: Implement initialisation
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.model = None
+
+    def fit(self, data: pd.DataFrame, **kwargs) -> "XGBoostModel":
+        """
+        Fit / train on historical data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data (OHLCV or feature matrix)
+
+        Returns
+        -------
+        self
+        """
+        # TODO: Implement training logic
+        raise NotImplementedError("fit() not yet implemented")
+
+    def predict(self, data: pd.DataFrame) -> pd.Series:
+        """
+        Generate predictions / signals.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data
+
+        Returns
+        -------
+        pd.Series
+            Predicted values or trading signals
+        """
+        # TODO: Implement prediction logic
+        raise NotImplementedError("predict() not yet implemented")
+
+    def evaluate(self, data: pd.DataFrame, labels: pd.Series) -> Dict[str, float]:
+        """
+        Evaluate performance on labelled data.
+
+        Returns dict of metric_name -> value.
+        """
+        # TODO: Implement evaluation metrics
+        raise NotImplementedError("evaluate() not yet implemented")
