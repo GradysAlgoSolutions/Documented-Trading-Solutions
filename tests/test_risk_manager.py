fae315e0f6dc6b7977370a93413a1e07cbe49216
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2022-05-06 21:53:58 +0000

    Implement risk manager unit tests

---
+++ tests/test_risk_manager.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+import pandas as pd
+
+
+class TestRiskManager:
+    """Unit tests for risk manager."""
+
+    def test_placeholder(self):
+        """Placeholder test — replace with real tests."""
+        # TODO: Implement tests for risk manager
+        assert True
+
+    def test_init(self):
+        """Test object initialisation."""
+        # TODO: Test that class initialises without errors
+        pass
+
+    def test_fit(self):
+        """Test fit method."""
+        # TODO: Create sample data and verify fit() runs
+        pass
