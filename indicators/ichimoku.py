f8b158c91bf1db2fc9ae9106bebb6ffd8362ccee
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2022-09-12 14:24:54 +0000

    Implement Ichimoku Cloud indicator

---
+++ indicators/ichimoku.py
@@ -0,0 +1,63 @@
+"""Ichimoku Cloud Indicator
+
+TODO: Replace stub with actual implementation.
+"""
+import numpy as np
+import pandas as pd
+
+
+class IchimokuCloud:
+    """
+    Ichimoku Cloud Indicator.
+
+    Parameters
+    ----------
+    (see __init__ for parameters)
+    """
+
+    def __init__(self, period: int = 14):
+        """Initialise Ichimoku Cloud indicator."""
+        # TODO: Implement initialisation
+        pass
+
+    def fit(self, data: pd.DataFrame, **kwargs) -> "IchimokuCloud":
+        """
+        Fit / train on historical data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data (OHLCV or feature matrix)
+
+        Returns
+        -------
+        self
+        """
+        # TODO: Implement training logic
+        raise NotImplementedError("fit() not yet implemented")
+
+    def predict(self, data: pd.DataFrame) -> pd.Series:
+        """
+        Generate predictions / signals.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data
+
+        Returns
+        -------
+        pd.Series
+            Predicted values or trading signals
+        """
+        # TODO: Implement prediction logic
+        raise NotImplementedError("predict() not yet implemented")
+
+    def evaluate(self, data: pd.DataFrame, labels: pd.Series) -> Dict[str, float]:
+        """
+        Evaluate performance on labelled data.
+
+        Returns dict of metric_name -> value.
+        """
+        # TODO: Implement evaluation metrics
+        raise NotImplementedError("evaluate() not yet implemented")
