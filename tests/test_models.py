efbab7e1e0b14475482b9745f7c7a0a4b0ad2a76
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2023-03-02 14:38:32 +0000

    Add model unit tests

---
+++ tests/test_models.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from typing import Optional, Tuple, Dict, Any
+from sklearn.base import BaseEstimator
+
+
+class TestModels:
+    """Unit tests for models."""
+
+    def test_placeholder(self):
+        """Placeholder test — replace with real tests."""
+        # TODO: Implement tests for models
+        assert True
+
+    def test_init(self):
+        """Test object initialisation."""
+        # TODO: Test that class initialises without errors
+        pass
+
+    def test_fit(self):
+        """Test fit method."""
+        # TODO: Create sample data and verify fit() runs
+        pass
