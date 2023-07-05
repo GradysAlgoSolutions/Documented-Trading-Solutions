f7873dadcc1d7e248fbd102c9738cfd03e31ec00
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2023-07-05 11:37:46 +0000

    Introduce indicator unit tests

---
+++ tests/test_indicators.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+
+class TestIndicators:
+    """Unit tests for indicators."""
+
+    def test_placeholder(self):
+        """Placeholder test — replace with real tests."""
+        # TODO: Implement tests for indicators
+        assert True
+
+    def test_init(self):
+        """Test object initialisation."""
+        # TODO: Test that class initialises without errors
+        pass
+
+    def test_fit(self):
+        """Test fit method."""
+        # TODO: Create sample data and verify fit() runs
+        pass
