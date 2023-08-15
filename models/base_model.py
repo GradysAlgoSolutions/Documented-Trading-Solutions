3b2b112847d3006a3a8aee2b895ada0f3d734791
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2023-08-15 21:44:57 +0000

    Add abstract base model

---
+++ models/base_model.py
@@ -0,0 +1,65 @@
+"""Abstract Base Model
+
+TODO: Replace stub with actual implementation.
+"""
+import numpy as np
+import pandas as pd
+from typing import Optional, Tuple, Dict, Any
+from sklearn.base import BaseEstimator
+
+
+class BaseModel:
+    """
+    Abstract Base Model.
+
+    Parameters
+    ----------
+    (see __init__ for parameters)
+    """
+
+    def __init__(self):
+        """Initialise abstract base model."""
+        # TODO: Implement initialisation
+        pass
+
+    def fit(self, data: pd.DataFrame, **kwargs) -> "BaseModel":
+        """
+        Fit / train on historical data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data (OHLCV or feature matrix)
+
+        Returns
+        -------
+        self
+        """
+        # TODO: Implement training logic
+        raise NotImplementedError("fit() not yet implemented")
+
+    def predict(self, data: pd.DataFrame) -> pd.Series:
+        """
+        Generate predictions / signals.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data
+
+        Returns
+        -------
+        pd.Series
+            Predicted values or trading signals
+        """
+        # TODO: Implement prediction logic
+        raise NotImplementedError("predict() not yet implemented")
+
+    def evaluate(self, data: pd.DataFrame, labels: pd.Series) -> Dict[str, float]:
+        """
+        Evaluate performance on labelled data.
+
+        Returns dict of metric_name -> value.
+        """
+        # TODO: Implement evaluation metrics
+        raise NotImplementedError("evaluate() not yet implemented")
