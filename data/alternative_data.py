725839ee18c6e4a07a1781ae16172019c45cdc67
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2023-08-25 22:29:20 +0000

    Set up alternative data sources

---
+++ data/alternative_data.py
@@ -0,0 +1,64 @@
+"""Alternative Data Sources
+
+TODO: Replace stub with actual implementation.
+"""
+import numpy as np
+import pandas as pd
+from typing import Optional, List, Tuple
+
+
+class AlternativeData:
+    """
+    Alternative Data Sources.
+
+    Parameters
+    ----------
+    (see __init__ for parameters)
+    """
+
+    def __init__(self):
+        """Initialise alternative data sources."""
+        # TODO: Implement initialisation
+        pass
+
+    def fit(self, data: pd.DataFrame, **kwargs) -> "AlternativeData":
+        """
+        Fit / train on historical data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data (OHLCV or feature matrix)
+
+        Returns
+        -------
+        self
+        """
+        # TODO: Implement training logic
+        raise NotImplementedError("fit() not yet implemented")
+
+    def predict(self, data: pd.DataFrame) -> pd.Series:
+        """
+        Generate predictions / signals.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data
+
+        Returns
+        -------
+        pd.Series
+            Predicted values or trading signals
+        """
+        # TODO: Implement prediction logic
+        raise NotImplementedError("predict() not yet implemented")
+
+    def evaluate(self, data: pd.DataFrame, labels: pd.Series) -> Dict[str, float]:
+        """
+        Evaluate performance on labelled data.
+
+        Returns dict of metric_name -> value.
+        """
+        # TODO: Implement evaluation metrics
+        raise NotImplementedError("evaluate() not yet implemented")
