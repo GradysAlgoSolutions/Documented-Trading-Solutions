2d3b9ba5bfd0fb9a987ef26bb576cd9bf5fb16b8
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2023-12-11 20:33:19 +0000

    Implement strategy unit tests

---
+++ tests/test_strategies.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from typing import Optional, Dict, Any
+from .base_strategy import BaseStrategy
+
+
+class TestStrategies:
+    """Unit tests for strategies."""
+
+    def test_placeholder(self):
+        """Placeholder test — replace with real tests."""
+        # TODO: Implement tests for strategies
+        assert True
+
+    def test_init(self):
+        """Test object initialisation."""
+        # TODO: Test that class initialises without errors
+        pass
+
+    def test_fit(self):
+        """Test fit method."""
+        # TODO: Create sample data and verify fit() runs
+        pass
