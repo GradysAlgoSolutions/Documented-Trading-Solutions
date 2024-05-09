ca4343ca6eeeb2c9fa2323d63042904bc3764683
GradysAlgoSolutions <GradysAlgoSolutions@users.noreply.github.com>
2024-05-09 10:14:24 +0000

    Add abstract base strategy

---
+++ strategies/base_strategy.py
@@ -0,0 +1,64 @@
+"""Abstract Base Strategy
+
+TODO: Replace stub with actual implementation.
+"""
+import numpy as np
+import pandas as pd
+from typing import Optional, Dict, Any
+
+
+class BaseStrategy:
+    """
+    Abstract Base Strategy.
+
+    Parameters
+    ----------
+    (see __init__ for parameters)
+    """
+
+    def __init__(self):
+        """Initialise abstract base strategy."""
+        # TODO: Implement initialisation
+        pass
+
+    def fit(self, data: pd.DataFrame, **kwargs) -> "BaseStrategy":
+        """
+        Fit / train on historical data.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data (OHLCV or feature matrix)
+
+        Returns
+        -------
+        self
+        """
+        # TODO: Implement training logic
+        raise NotImplementedError("fit() not yet implemented")
+
+    def predict(self, data: pd.DataFrame) -> pd.Series:
+        """
+        Generate predictions / signals.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            Input data
+
+        Returns
+        -------
+        pd.Series
+            Predicted values or trading signals
+        """
+        # TODO: Implement prediction logic
+        raise NotImplementedError("predict() not yet implemented")
+
+    def evaluate(self, data: pd.DataFrame, labels: pd.Series) -> Dict[str, float]:
+        """
+        Evaluate performance on labelled data.
+
+        Returns dict of metric_name -> value.
+        """
+        # TODO: Implement evaluation metrics
+        raise NotImplementedError("evaluate() not yet implemented")
